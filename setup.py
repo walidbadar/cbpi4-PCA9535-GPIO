386a660502f7b7a401083961d03ac12f5ed74ba4
Waleed Badar <81442784+walidbadar@users.noreply.github.com>
2022-09-17 22:54:46 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,18 +6,18 @@ this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-setup(name='cbpi4-PCF8574-GPIO',
+setup(name='cbpi4-PCA9535-GPIO',
       version='0.0.5',
-      description='CraftBeerPi4 PCF8574 Actor Plugin',
-      author='Alexander Vollkopf',
+      description='CraftBeerPi4 PCA9535 Actor Plugin',
+      author='Waleed Badar',
       author_email='avollkopf@web.de',
-      url='https://github.com/avollkopf/cbpi4-PCF8574-GPIO',
+      url='https://github.com/walidbadar/cbpi4-PCA9535-GPIO',
       include_package_data=True,
       package_data={
         # If any package contains *.txt or *.rst files, include them:
       '': ['*.txt', '*.rst', '*.yaml'],
-      'cbpi4-PCF8574-GPIO': ['*','*.txt', '*.rst', '*.yaml']},
-      packages=['cbpi4-PCF8574-GPIO'],
+      'cbpi4-PCA9535-GPIO': ['*','*.txt', '*.rst', '*.yaml']},
+      packages=['cbpi4-PCA9535-GPIO'],
       install_requires=[
       'smbus2',
       'pcf8574-io'
